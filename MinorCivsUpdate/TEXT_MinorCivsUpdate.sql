3777ba570ecedc0f97ebbc2cb3e5118af6fa1a2e
Xiaojian ZHENG <zhengjian.au@gmail.com>
2023-04-11 17:53:23 +0930

    more improvements can be built on features

--- MinorCivsUpdate/TEXT_MinorCivsUpdate.sql
+++ MinorCivsUpdate/TEXT_MinorCivsUpdate.sql
@@ -1,7 +1,7 @@
 --MinorCivs
 INSERT OR REPLACE INTO EnglishText (Tag,Text) VALUES
-('LOC_SCIENCE_AT_PEACE_BONUS_ZJ',
-'At peace with all civilizations.'),
+('LOC_TECH_BUTTRESS_DESCRIPTION_ZJ',
+'Improvements can build on floodplains.'),
 
 ('LOC_IMPROVEMENT_BATEY_DESCRIPTION_ZJ',
 'Unlocks the Builder ability to construct a Batey.[NEWLINE][NEWLINE]+2 [ICON_Culture] Culture. +1 [ICON_Culture] Culture for every adjacent Bonus Resource and Entertainment Complex (increased to +2 [Icon_Culture] Culture when Exploration is discovered). Provides [Icon_Tourism] Tourism after researching Flight. Cannot be built adjacent to another Batey or on Hills.');
