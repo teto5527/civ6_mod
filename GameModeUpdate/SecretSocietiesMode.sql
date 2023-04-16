7c1a9fb2cec40083451e0010b9eb3b744853e7cf
Xiaojian ZHENG <zhengjian.au@gmail.com>
2023-04-17 00:03:23 +0930

    ley line no longer on snow

--- GameModeUpdate/SecretSocietiesMode.sql
+++ GameModeUpdate/SecretSocietiesMode.sql
@@ -37,19 +37,9 @@ INSERT OR REPLACE INTO ModifierArguments (ModifierId, Name, Value) VALUES
 ('SECRET_SOCIETIES_VAMPIRE_HEAL_AFTER_KILL', 'Amount', '25');
 
 --Hermetic Order 
---lv1: every Ley Line in city +5% all yield
--- UPDATE GovernorPromotions
---     SET Description='LOC_GOVERNOR_PROMOTION_HERMETIC_ORDER_1_ZJ'
---     WHERE GovernorPromotionType='GOVERNOR_PROMOTION_HERMETIC_ORDER_1';
--- INSERT OR REPLACE INTO GameModifiers (ModifierId) VALUES
--- ('GOVERNOR_PROMOTION_HERMETIC_ORDER_1_CITY_PER_LEY_LINE_YIELD');
--- INSERT OR REPLACE INTO GovernorPromotionModifiers (GovernorPromotionType, ModifierId) VALUES 
--- ('GOVERNOR_PROMOTION_HERMETIC_ORDER_1', 'GOVERNOR_PROMOTION_HERMETIC_ORDER_1_CITY_PER_LEY_LINE_YIELD');
--- INSERT OR REPLACE INTO Modifiers (ModifierId, ModifierType, RunOnce, Permanent, NewOnly, OwnerRequirementSetId, SubjectRequirementSetId) VALUES 
--- ('GOVERNOR_PROMOTION_OWLS_OF_MINERVA_1_PER_TRIBUTARY_GOLD', 'MODIFIER_PLAYER_ADJUST_YIELD_MODIFIER_PER_TRIBUTARY', 0, 0, 0, NULL, NULL);
--- INSERT OR REPLACE INTO ModifierArguments (ModifierId, Name, Value) VALUES 
--- ('GOVERNOR_PROMOTION_OWLS_OF_MINERVA_1_PER_TRIBUTARY_GOLD', 'YieldType', 'YIELD_GOLD'),
--- ('GOVERNOR_PROMOTION_OWLS_OF_MINERVA_1_PER_TRIBUTARY_GOLD', 'Amount', '5');
+--lv1: ley line no longer on snow 
+DELETE FROM Feature_ValidTerrains WHERE FeatureType = 'RESUORCES_LEY_LINE' AND TerrainType = 'TERRAIN_SNOW';
+DELETE FROM Feature_ValidTerrains WHERE FeatureType = 'RESUORCES_LEY_LINE' AND TerrainType = 'TERRAIN_SNOW_HILLS';
 
 --LV2
 -- INSERT OR REPLACE INTO Building_YieldChanges (BuildingType, YieldType, YieldChange) VALUES('BUILDING_ALCHEMICAL_SOCIETY', 'YIELD_SCIENCE', 6);
