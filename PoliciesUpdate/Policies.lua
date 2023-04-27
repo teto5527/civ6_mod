6219eb8b24bff776b957305da224af51dffb8c61
Xiaojian ZHENG <zhengjian.au@gmail.com>
2023-04-27 12:19:11 +0930

    add new policy [GIFT_EXCHANGE]

---
+++ PoliciesUpdate/Policies.lua
@@ -0,0 +1,11 @@
+function DelegationAddGold (eReactingPlayer:number, eActingPlayer:number, values:table)
+    local player = Players[eActingPlayer];
+    local policyID = GameInfo.Policies["POLICY_GIFT_EXCHANGE"].Index
+    if values.StatementType == -883488896 then
+        if player:GetCulture():IsPolicyActive(policyID) then
+            player:GetTreasury():ChangeGoldBalance(15);
+        end
+    end
+end
+
+Events.DiplomacyStatement.Add(DelegationAddGold);
