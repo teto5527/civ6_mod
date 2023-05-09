18f4d22e16b3d24ab3ccbd0a7a075382981234bf
Xiaojian ZHENG <zhengjian.au@gmail.com>
2023-05-09 20:35:02 +0930

    Vampire nerf

--- GameModeUpdate/TEXT_GameModeUpdate.sql
+++ GameModeUpdate/TEXT_GameModeUpdate.sql
@@ -1,5 +1,5 @@
 INSERT OR REPLACE INTO EnglishText (Tag,Text) VALUES
 ('LOC_GOVERNOR_PROMOTION_SANGUINE_PACT_1_DESCRIPTION_ZJ',
-'Unlocked by destroying a Barbarian Camp. +1 Military policy slot. Grants a Vampire unit in your [ICON_Capital] Capital.[NEWLINE][NEWLINE]Vampires +3 Movement if starting turn in friendly territory.[NEWLINE]Vampires heal up to 25 hit points when they eliminate a unit.'),
+'Unlocked by destroying a Barbarian Camp. +1 Military policy slot. Grants a Vampire unit in your [ICON_Capital] Capital.[NEWLINE][NEWLINE]Vampires +3 Movement if starting turn in friendly territory.[NEWLINE]Vampires heal up to 20 hit points when they eliminate a unit.'),
 ('LOC_GOVERNOR_PROMOTION_OWLS_OF_MINERVA_1_DESCRIPTION_ZJ',
 'Unlocked by sending an [ICON_Envoy] Envoy to a City-State. +1 Economic policy slot. Each [ICON_TradeRoute] Trade Route sent to a City-State awards 1 [ICON_Envoy] Envoy there.[NEWLINE][NEWLINE]+2% [ICON_Gold] Gold per city-state you are the Suzerain of.');
