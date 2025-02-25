b03fffe6ac37e42b46dd8c7764a71243d8b7160f
James ZHENG <zhengjian.au@gmail.com>
2025-02-25 12:57:23 +1030

    Refactor code for better readability

--- PoliciesUpdate/Policies.lua
+++ PoliciesUpdate/Policies.lua
@@ -1,4 +1,13 @@
 function DelegationAddGold (eReactingPlayer:number, eActingPlayer:number, values:table)
+    print("=== DelegationAddGold Debug ===")
+    print("eReactingPlayer:", eReactingPlayer)
+    print("eActingPlayer:", eActingPlayer)
+    print("values table contents:")
+    for k,v in pairs(values) do
+        print(k, ":", v)
+    end
+    print("========================")
+
     local player = Players[eActingPlayer];
     local policyID = GameInfo.Policies["POLICY_TRIBUTE"].Index
     if values.StatementType == -883488896 then
