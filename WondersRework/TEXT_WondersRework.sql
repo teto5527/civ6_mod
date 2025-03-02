92b64e53f5f475921a5721b22211f7a433476c22
James ZHENG <zhengjian.au@gmail.com>
2025-03-02 11:01:44 +1030

    Add Taj Mahal and Univertiy of Sankore changes

--- WondersRework/TEXT_WondersRework.sql
+++ WondersRework/TEXT_WondersRework.sql
@@ -1,4 +1,7 @@
 INSERT OR REPLACE INTO EnglishText (Tag,Text) VALUES
+('LOC_BUILDING_TAJ_MAHAL_DESCRIPTION_ZJ',
+'+1 Era Score from Historic Moment earned after this wonder is complete if that Moment is usually worth 2 or more Era Score. When you at Golden age, +10% [ICON_TOURISM] Tourism across your empire. Must be built next to a River.'),
+
 ('LOC_BUILDING_CHICHEN_ITZA_DESCRIPTION_ZJ',
 '+2 [ICON_Culture] Culture to all Rainforest tiles nationwide. Rainforest no longer -1 appeal to the adjacent tiles. Must be built on Rainforest.'),
 
@@ -11,4 +14,7 @@ INSERT OR REPLACE INTO EnglishText (Tag,Text) VALUES
 ('LOC_BUILDING_STONEHENGE_DESCRIPTION_ZJ',
 'Grants a free [ICON_GREATPROPHET] Great Prophet. [ICON_GREATPROPHET] Great Prophets may found a Religion on Stonehenge instead of a Holy Site. Must be adjacent to [ICON_Resource_Stone] Stone and on flat land. All quarry +1 [ICON_Faith] Faith.'),
 ('LOC_BUILDING_GREAT_LIGHTHOUSE_DESCRIPTION_ZJ',
-'+1 [ICON_Movement] Movement for all naval units. Grant a lighthouse buidling in the city.[NEWLINE][NEWLINE]Must be built on the Coast, and adjacent to land and a Harbor district.');
+'+1 [ICON_Movement] Movement for all naval units. Grant a lighthouse buidling in the city.[NEWLINE][NEWLINE]Must be built on the Coast, and adjacent to land and a Harbor district.'),
+
+('LOC_BUILDING_UNIVERSITY_SANKORE_DESCRIPTION_ZJ',
+'+1 [ICON_Science] Science and +2 [ICON_Gold] Gold from [ICON_TRADEROUTE] Trade Routes, and +1 [ICON_Faith] Faith if it is domestic Trade Route.[NEWLINE][NEWLINE]Must be built on the Desert or Desert Hills, and adjacent to a Campus district with a University.');
