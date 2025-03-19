ff7e09504346f8861114fc6bc9f5c9b687a2dc3b
James ZHENG <zhengjian.au@gmail.com>
2025-03-20 10:11:10 +1030

    Delete Mansa Musa's LA changes

--- LeadersUpdate/TEXT_LeadersUpdate.sql
+++ LeadersUpdate/TEXT_LeadersUpdate.sql
@@ -18,7 +18,4 @@ INSERT OR REPLACE INTO EnglishText (Tag,Text) VALUES
 'One extra Economic policy slot in any government. Receive a random [ICON_CIVICBOOSTED] Eureka and [ICON_TECHBOOSTED] Inspiration when establishing a [ICON_TradingPost] Trading Post in another Civilizations city for the first time. Encampment +1 [ICON_TRADEROUTE] Trade Route capacity. Each Encampment in your empire make the outgoing international [ICON_TRADEROUTE] Trade Routes +3 [ICON_Gold] Gold.'),
 
 ('LOC_CIVILIZATION_LAST_PROPHET_DESCRIPTION_ZJ', -- Saladin
-"Automatically receive the final [ICON_GREATPROPHET] Great Prophet when the next-to-last one is claimed (if you have not earned a [ICON_GREATPROPHET] Great Prophet already). +1 [ICON_Science] Science for each foreign city following Arabia's Religion. Holy Site give Culture Bomb."),
-
-('LOC_TRAIT_LEADER_SAHEL_MERCHANTS_DESCRIPTION_ZJ', -- Mansa
-'International [ICON_TRADEROUTE] Trade Routes gain +1 [ICON_Gold] Gold for every flat Desert tile in the origin city. Receive +1 [ICON_TRADEROUTE] Trade Capacity first time you enter a Golden Age. +2 [ICON_Gold] Gold from Desert.');
+"Automatically receive the final [ICON_GREATPROPHET] Great Prophet when the next-to-last one is claimed (if you have not earned a [ICON_GREATPROPHET] Great Prophet already). +1 [ICON_Science] Science for each foreign city following Arabia's Religion. Holy Site give Culture Bomb.");
