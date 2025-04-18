08c4d7b2ee00fef9132840abaa1201bdc4f0fba5
James ZHENG <zhengjian.au@gmail.com>
2025-04-18 22:42:17 +0930

    Fix Russia's UA bug

--- CivilizationsUpdate/RussiaRework.sql
+++ CivilizationsUpdate/RussiaRework.sql
@@ -2,6 +2,17 @@ UPDATE Districts SET Description = 'LOC_DISTRICT_LAVRA_DESCRIPTION_ZJ' WHERE Dis
 
 UPDATE District_GreatPersonPoints SET PointsPerTurn = '1' WHERE DistrictType = 'DISTRICT_LAVRA';
 
-UPDATE ModifierArguments SET Value = 'GREAT_PERSON_CLASS_PROPHET' WHERE ModifierId = 'TRAIT_SHRINE_WRITING_POINTS';
-UPDATE ModifierArguments SET Value = 'GREAT_PERSON_CLASS_WRITER' WHERE ModifierId = 'TRAIT_TEMPLE_ARTIST_POINTS';
-UPDATE ModifierArguments SET Value = 'GREAT_PERSON_CLASS_ARTIST' WHERE ModifierId = 'TRAIT_TIER3_MUSICIAN_POINTS';
+UPDATE ModifierArguments 
+SET Value = 'GREAT_PERSON_CLASS_PROPHET'
+WHERE ModifierId = 'TRAIT_SHRINE_WRITING_POINTS' 
+AND Name = 'GreatPersonClassType';
+
+UPDATE ModifierArguments 
+SET Value = 'GREAT_PERSON_CLASS_WRITER'
+WHERE ModifierId = 'TRAIT_TEMPLE_ARTIST_POINTS' 
+AND Name = 'GreatPersonClassType';
+
+UPDATE ModifierArguments 
+SET Value = 'GREAT_PERSON_CLASS_ARTIST'
+WHERE ModifierId = 'TRAIT_TIER3_MUSICIAN_POINTS' 
+AND Name = 'GreatPersonClassType';
