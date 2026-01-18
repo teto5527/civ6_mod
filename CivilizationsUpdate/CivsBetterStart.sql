ea3ca718aca40c6aeda465c0c08b5ea3399d56df
James ZHENG <zhengjian.au@gmail.com>
2026-01-18 23:26:23 +1030

    Update Hungary change implementation

--- CivilizationsUpdate/CivsBetterStart.sql
+++ CivilizationsUpdate/CivsBetterStart.sql
@@ -3,3 +3,6 @@ INSERT OR REPLACE INTO StartBiasResources (CivilizationType, ResourceType, Tier)
 
 INSERT OR REPLACE INTO StartBiasTerrains (CivilizationType, TerrainType, Tier) VALUES
 ('CIVILIZATION_ENGLAND', 'TERRAIN_COAST' ,'2'); --England
+
+INSERT OR REPLACE INTO StartBiasFeatures (CivilizationType, FeatureType, Tier) VALUES
+('CIVILIZATION_HUNGARY', 'FEATURE_GEOTHERMAL_FISSURE' ,'2'); --Hungary
